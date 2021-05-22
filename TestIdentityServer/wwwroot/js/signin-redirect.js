4252912c226e63c549f74e854afc8673d70efe42
Jose Carlos Nuñez Martinez <josecdom94@gmail.com>
2021-05-22 08:38:16 +0200

    Redirect Button

--- TestIdentityServer/wwwroot/js/signin-redirect.js
+++ TestIdentityServer/wwwroot/js/signin-redirect.js
@@ -1,1 +1,14 @@
-window.location.href = document.querySelector("meta[http-equiv=refresh]").getAttribute("data-url");
+var goToRedirectURl = document.querySelector("meta[http-equiv=refresh]").getAttribute("data-url");
+
+window.location.href = goToRedirectURl;
+
+var redirectButton = document.getElementById("myButton")
+if (!!myButton) {
+    redirectButton.onclick = myFunction;
+}
+
+function myFunction() {
+    window.location.href = goToRedirectURl;
+}
+
+
